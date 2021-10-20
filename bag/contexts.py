9b4afa50b0fee03641fd8111612500ce228a8292
MichelleCoffey <michellembcoffey@icloud.com>
2021-10-20 08:22:19 +0000

    fixed issue with bag.

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,5 @@
+
+   
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
@@ -8,7 +10,7 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-    bag = request.session.get('bag',{})
+    bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
@@ -36,15 +38,15 @@ def bag_contents(request):
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
-        delivery = 0 
-        free_delivery_delta = 0 
-
+        delivery = 0
+        free_delivery_delta = 0
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count, 
+        'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
