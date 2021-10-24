73b00aaa042dd7975d3d74c004a63aa3cd6e054e
MichelleCoffey <michellembcoffey@icloud.com>
2021-10-24 11:51:00 +0000

    addeed stripe css and some functionality.

--- checkout/views.py
+++ checkout/views.py
@@ -10,11 +10,12 @@ def checkout(request):
         message.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jo1l7GqHuyYmd1vmd6cYBAE428f8Hm1Lz2vyyfbjQh1psTJlYLml6gEDSC3j5NpxygvCkW1n4JnlcOq916TeSEz00Gta8jaZP',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
